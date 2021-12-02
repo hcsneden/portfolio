225e173c5130b3b651b1c852b1c86402c5514461
hcsneden <48335502+hcsneden@users.noreply.github.com>
2021-12-02 15:23:36 -0600

    initial check in

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    title: "Software Engineer",
+    subtitle: "Full Stack",
+    description:
+      "I have been at Travelers Insurance since 2017 as a full stack engineer with focus on MERN stack applications.",
+    image: "./Travelers-Logo.jpeg",
+    link: "",
+  },
+  {
+    title: "UX/UI",
+    subtitle: "University of MN Bootcamp",
+    description:
+      "I am currently studying UX and UI design at the University of MN Bootcamp, which I will complete in June 2022.",
+    image: "./ux.png",
+    link: "",
+  }
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Adobe XD",
+  "Node",
+  "MongoDB",
+  "Invision",
+];
